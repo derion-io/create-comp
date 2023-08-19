4d6466051e41aac1f45303e41f464b5b527ee0bb
khanhpham <khanhpham08121996@gmail.com>
2023-08-19 16:54:29 +0700

    UnderConstruction: update styles

--- src/Components/UnderConstruction/index.tsx
+++ src/Components/UnderConstruction/index.tsx
@@ -4,8 +4,8 @@ import { DerivableIcon } from '../ui/Icon'
 import { Text } from '../ui/Text'
 
 export const UnderConstruction = () => {
-  return <div className='page-loading-indicator__wrap'>
-    <div className='page-loading-indicator__logo'>
+  return <div className='under-construction__wrap'>
+    <div className='under-construction__logo'>
       <DerivableIcon width={360} height={60} />
     </div>
     <Text fontWeight={700} fontSize={22}>UNDER CONSTRUCTION</Text>
